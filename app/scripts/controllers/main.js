27f1c38ebdc04a22d4e54f0d518195448039a457
Kyle Clark <kyle.clark@meltmedia.com>
2014-04-13 14:44:11 -0700

    Removed default yeoman values; added gameBoard model and method to initialize 5x5 array of 0 or 1 values; applied associated unit tests; minor style update to be more readable; updated html to show initial randomized gameBoard and other text

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,31 @@
 
 angular.module('gameOfLifeApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.gameBoard = null;
+
+    function init () {
+      $scope.initGameBoard();
+    }
+
+    /* Initialize gameBoard array */
+    $scope.initGameBoard = function () {
+      var GAME_BOARD_SIZE = 5,
+          gameBoard = new Array(GAME_BOARD_SIZE),
+          row = null,
+          col = null;
+
+      // Create gameBoard array of rows and column cells
+      for (row = 0; row < GAME_BOARD_SIZE; row++) {
+        gameBoard[row] = new Array(GAME_BOARD_SIZE);
+
+        for (col = 0; col < GAME_BOARD_SIZE; col++) {
+          // Round number to a random generation between 0 < 1
+          gameBoard[row][col] = Math.round(Math.random());
+        }
+      }
+
+      $scope.gameBoard = gameBoard;
+    };
+
+    init();
   });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,22 +1,51 @@
-'use strict';
+"use strict";
 
-describe('Controller: MainCtrl', function () {
+describe("Controller: MainCtrl", function () {
 
   // load the controller's module
-  beforeEach(module('gameOfLifeApp'));
+  beforeEach(module("gameOfLifeApp"));
 
   var MainCtrl,
-    scope;
+      scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    MainCtrl = $controller("MainCtrl", {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).to.equal(3);
+  describe("When initializing a gameBoard", function () {
+
+    it("should create a 5x5 gameBoard array", function () {
+      // Act
+      scope.initGameBoard();
+
+      // Assert
+      expect(scope.gameBoard).to.exist;
+      expect(scope.gameBoard).to.be.instanceof(Array);
+      expect(scope.gameBoard[4][4]).to.exist;
+    });
+
+    it("should populate each gameBoard cell with a value of 0 or 1", function () {
+      // Arrange
+      var GAME_BOARD_SIZE = 5,
+          row = null,
+          col = null;
+
+      // Act
+      scope.initGameBoard();
+
+      // Assert
+      for (row = 0; row < GAME_BOARD_SIZE; row++) {
+        for (col = 0; col < GAME_BOARD_SIZE; col++) {
+          expect(scope.gameBoard[row][col]).to.be.at.least(0);
+          expect(scope.gameBoard[row][col]).to.be.at.most(1);
+        }
+      }
+    });
+
   });
+
 });
