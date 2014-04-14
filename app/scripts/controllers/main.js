493a67622f0eec9a18503cd30340b3039f91e586
Kyle Clark <kyle.clark@meltmedia.com>
2014-04-13 20:39:10 -0700

    Updated html to repeat through gameBoards array container of gameBoard arrays; updated MainCtrl to scaffold implementation for createNewGeneration; created common ArrayService method to handle cloning multi dimensional arrays; applied MainCtrl, CommonArrayService unit tests; minor css tweaks for temporary styling and formatting

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,32 +1,78 @@
 'use strict';
 
-angular.module('gameOfLifeApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.gameBoard = null;
+angular.module('gameOfLifeApp', [
+  'Common.ArrayService'
+])
+    .controller('MainCtrl', function ($scope, CommonArrayService) {
 
-    function init () {
+    /* Init main flow */
+    function init() {
+      $scope.initVars();
       $scope.initGameBoard();
-    }
+      $scope.createNewGeneration();
+    };
+
+    /* Init vars within scope */
+    $scope.initVars = function () {
+      $scope.GAME_BOARD_SIZE = 5;
+    };
 
     /* Initialize gameBoard array */
     $scope.initGameBoard = function () {
-      var GAME_BOARD_SIZE = 5,
-          gameBoard = new Array(GAME_BOARD_SIZE),
+      var GAME_BOARD_SIZE = $scope.GAME_BOARD_SIZE,
           row = null,
-          col = null;
+          col = null,
+          colSize = GAME_BOARD_SIZE,
+          rowSize = GAME_BOARD_SIZE,
+          gameBoard = new Array(GAME_BOARD_SIZE);
 
       // Create gameBoard array of rows and column cells
-      for (row = 0; row < GAME_BOARD_SIZE; row++) {
-        gameBoard[row] = new Array(GAME_BOARD_SIZE);
+      for (row = 0; row < rowSize; row++) {
+        gameBoard[row] = new Array(this.rowSize);
 
-        for (col = 0; col < GAME_BOARD_SIZE; col++) {
-          // Round number to a random generation between 0 < 1
-          gameBoard[row][col] = Math.round(Math.random());
+         for (col = 0; col < colSize; col++) {
+           // Round number to a random generation between 0 < 1
+           gameBoard[row][col] = Math.round(Math.random());
+         }
+      }
+
+      // Init empty gameBoards array and push initial randomized gameBoard to gameBoards
+      $scope.gameBoards = [];
+      $scope.gameBoards.push(gameBoard);
+    };
+
+    /* Create a new generation of cell values to push to gameBoards array */
+    $scope.createNewGeneration = function () {
+      var GAME_BOARD_SIZE = $scope.GAME_BOARD_SIZE,
+          gameBoardsLength = $scope.gameBoards.length,
+          row = null,
+          col = null,
+          cell = null,
+          colSize = GAME_BOARD_SIZE,
+          rowSize = GAME_BOARD_SIZE,
+          childGenerationBoard = null;
+
+      // Clone array copy of parentGeneration to childGeneration
+      $scope.parentGenerationBoard = $scope.gameBoards[gameBoardsLength - 1];
+      childGenerationBoard = CommonArrayService.cloneMultiDimensionalArray($scope.parentGenerationBoard);
+
+      // Create gameBoard array of rows and column cells
+      for (row = 0; row < rowSize; row++) {
+        for (col = 0; col < colSize; col++) {
+          cell = childGenerationBoard[row][col];
+          childGenerationBoard[row][col] = $scope.evolveCell(row, col, cell);
         }
       }
 
-      $scope.gameBoard = gameBoard;
+      // Push new childGeneration game board to gameBoards
+      $scope.gameBoards.push(childGenerationBoard);
     };
 
+    /* Logic to determine cell values of evolution generation */
+    $scope.evolveCell = function (row, col, cell) {
+      return 1;
+    };
+
+    // Init main flow
     init();
   });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,49 +1,153 @@
-"use strict";
+'use strict';
 
-describe("Controller: MainCtrl", function () {
+describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module("gameOfLifeApp"));
-
-  var MainCtrl,
-      scope;
+  var MainCtrl = null,
+      scope = null;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller("MainCtrl", {
-      $scope: scope
+  beforeEach(function () {
+    module('gameOfLifeApp');
+
+    inject(function ($controller, $rootScope) {
+      scope = $rootScope.$new();
+      MainCtrl = $controller('MainCtrl', {
+        $scope: scope
+      });
     });
-  }));
+  });
 
-  describe("When initializing a gameBoard", function () {
+  describe('When initializing variables', function () {
 
-    it("should create a 5x5 gameBoard array", function () {
+    it('should set values of scope variables', function () {
+      // Act
+      scope.initVars();
+
+      // Assert
+      expect(scope.GAME_BOARD_SIZE).to.equal(5);
+    });
+
+  });
+
+  describe('When initializing a gameBoard', function () {
+
+    it('should define an empty gameBoards array container', function () {
       // Act
       scope.initGameBoard();
 
       // Assert
-      expect(scope.gameBoard).to.exist;
-      expect(scope.gameBoard).to.be.instanceof(Array);
-      expect(scope.gameBoard[4][4]).to.exist;
+      expect(scope.gameBoards).to.exist;
+      expect(scope.gameBoards).to.be.instanceof(Array);
     });
 
-    it("should populate each gameBoard cell with a value of 0 or 1", function () {
+    it('should push a new 5x5 game board array on to gameBoards array container', function () {
+      var gameBoard = null;
+
+      // Act
+      scope.initGameBoard();
+      gameBoard = scope.gameBoards[0];
+
+      // Assert
+      expect(scope.gameBoards[0]).to.exist;
+      expect(scope.gameBoards[0]).to.be.instanceof(Array);
+      expect(gameBoard[4][4]).to.exist;
+    });
+
+    it('should populate each game board cell with a value of 0 or 1', function () {
       // Arrange
       var GAME_BOARD_SIZE = 5,
+          gameBoard = null,
           row = null,
           col = null;
 
       // Act
       scope.initGameBoard();
+      gameBoard = scope.gameBoards[0];
 
       // Assert
       for (row = 0; row < GAME_BOARD_SIZE; row++) {
         for (col = 0; col < GAME_BOARD_SIZE; col++) {
-          expect(scope.gameBoard[row][col]).to.be.at.least(0);
-          expect(scope.gameBoard[row][col]).to.be.at.most(1);
+          expect(gameBoard[row][col]).to.be.at.least(0);
+          expect(gameBoard[row][col]).to.be.at.most(1);
         }
       }
+    });
+
+  });
+
+  describe('When creating a new generation on the game board', function () {
+
+    it('should assign the last gameBoard in gameBoards array container to scope.parentGenerationBoard', function () {
+      // Arrange
+      var gameBoard = null,
+          gameBoardsLength = null;
+
+      scope.initGameBoard();
+      gameBoardsLength = scope.gameBoards.length;
+      gameBoard = scope.gameBoards[gameBoardsLength - 1];
+
+      // Act
+      scope.createNewGeneration();
+
+      // Assert
+      expect(scope.parentGenerationBoard).to.exist;
+      expect(scope.parentGenerationBoard).to.eql(gameBoard);
+    });
+
+    it('should push a new 5x5 game board array on to gameBoards array container', function () {
+      // Arrange
+      var gameBoardsLength = null,
+          newGameBoardsLength = null;
+
+      scope.initGameBoard();
+      gameBoardsLength = scope.gameBoards.length;
+
+      // Act
+      scope.createNewGeneration();
+      newGameBoardsLength = scope.gameBoards.length;
+
+      // Assert
+      expect(scope.gameBoards[gameBoardsLength]).to.exist;
+      expect(scope.gameBoards[gameBoardsLength]).to.be.instanceof(Array);
+      expect(newGameBoardsLength).to.equal(gameBoardsLength + 1);
+    });
+
+    it('should evolve each cell in the new game board to a value of 1', function () {
+      // Arrange
+      var GAME_BOARD_SIZE = 5,
+          gameBoardsLength = null,
+          gameBoard = null,
+          row = null,
+          col = null;
+
+      scope.initGameBoard();
+      gameBoardsLength = scope.gameBoards.length;
+
+      // Act
+      scope.createNewGeneration();
+      gameBoard = scope.gameBoards[gameBoardsLength];
+
+      // Assert
+      for (row = 0; row < GAME_BOARD_SIZE; row++) {
+        for (col = 0; col < GAME_BOARD_SIZE; col++) {
+          expect(gameBoard[row][col]).to.equal(1);
+        }
+      }
+    });
+
+  });
+
+  describe('When the evolveCell method is called', function () {
+
+    it('should return a value of 1', function () {
+      // Arrange
+      var cell = null;
+
+      // Act
+      cell = scope.evolveCell();
+
+      // Assert
+      expect(cell).to.equal(1);
     });
 
   });
